6dabd3704f5063755ed53d9c7737dd373e572dc8
gamepad947 <44636488+gamepad947@users.noreply.github.com>
2018-11-13 00:45:31 +0300

    Create data.js

---
+++ familyTree/js/data.js
@@ -0,0 +1,102 @@
+var treeData = {
+	id: 1,
+	name: "Konstantin",
+	wife: "Elena",
+	status: "father",
+	children: [
+		{
+			id: 4,
+			name: 'Oleg',
+			wife: "Oksana",
+			status: "father",
+			children: [
+				{
+					id: 10,
+					name: "Nikita",
+					wife: "Kristina",
+					status: "father",
+					children: [
+						{
+							id: 19,
+							name: "Ksenia",
+							husband: "",
+							children: []
+						},
+						{
+							id: 20,
+							name: "Alesya",
+							husband: "",
+							children: []
+
+						}
+					]
+				},
+				{
+					id: 12,
+					name: "Alexandr",
+					wife: "Julia",
+					status: "father",
+					children: [
+						{
+							id: 21,
+							name: "Vyacheslav",
+							wife: "",
+							children: []
+						},
+						{
+							id: 22,
+							name: "Valeria",
+							husband: "",
+							children: []
+
+						}
+
+					]
+				}
+			]
+		},
+		{
+			id: 6,
+			name: "Kolya",
+			wife: "Maria",
+			status: "father",
+			children: [
+				{
+					id: 14,
+					name: "Evgeniya",
+					husband: "Vitaliy",
+					children: [
+						{
+							id: 23,
+							name: "Igor",
+							wife: "",
+							children: []
+						},
+					]
+				},
+				{
+					id: 16,
+					name: "Roman",
+					wife: "Anastasiya",
+					status: "father",
+					children: [
+						{
+							id: 25,
+							name: "Elizaveta",
+							wife: "",
+							children: []
+						},
+						{
+							id: 26,
+							name: "Valeria",
+							husband: "",
+							children: []
+
+						}
+
+					]
+				}
+			]
+		}
+	]
+}

762f9591e28afb960943cf1500c5d41eeaf8b0ca
gamepad947 <44636488+gamepad947@users.noreply.github.com>
2018-11-23 16:59:30 +0300

    Update index.js

--- weather-forecast/src/index.js
+++ weather-forecast/src/index.js
@@ -1,6 +1,6 @@
-var loc = dataModul.getLocation(success, error);
+dataModule.getLocation(success, error);
 function success(pos) {
-  var data = dataModul.getData(`https://api.openweathermap.org/data/2.5/weather?
+  var data = dataModule.getData(`https://api.openweathermap.org/data/2.5/weather?
 	lat=${pos.coords.latitude}&
 	lon=${pos.coords.longitude}&
 	units=metric&APPID=1713de24b31c3508f06044a0598fa751`);

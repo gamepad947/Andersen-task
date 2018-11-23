76397b5653c50ca487546565b0d4e1d0d6b6ad54
gamepad947 <44636488+gamepad947@users.noreply.github.com>
2018-11-23 16:34:04 +0300

    Update model.js

--- weather-forecast/src/model.js
+++ weather-forecast/src/model.js
@@ -1,29 +1,19 @@
-var model = (function () {
-	function getLocation(success, error) {
-		if (navigator.geolocation) {
+var dataModul = (function () {
+  function getLocation(success, error) {
+    if (navigator.geolocation) {
 
-			navigator.geolocation.getCurrentPosition(success, error)
-		} else {
-			console.log('No location detected');
-		}
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      console.log('No location detected');
+    }
 
-	};
-	function query(url) {
-		fetch(url)
-			.then(function (response) {
-				if (response.ok) {
-					return response.json()
-				}
-			})
-			.then(function (data) {
-				view.render(data)
-				console.log(data);
-				return data;
-			})
-
-	}
-	return {
-		getLocation,
-		query
-	}
+  };
+  function getData(url) {
+    return fetch(url)
+      .then(response => response.json());
+  }
+  return {
+    getLocation,
+    getData
+  }
 })();
